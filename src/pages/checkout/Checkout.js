b3474f3dfafe18e5ed24196772e547ca8c6dccca
LocCuTo <phananhloc03102001@gmail.com>
2022-11-20 22:02:32 +0700

    push to heroku

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -35,7 +35,7 @@ const Checkout = () => {
 
     useEffect(() => {
         // Create PaymentIntent as soon as the page loads
-        fetch('http://localhost:4242/create-payment-intent', {
+        fetch('https://eshop-fullstack-app.herokuapp.com/create-payment-intent', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
