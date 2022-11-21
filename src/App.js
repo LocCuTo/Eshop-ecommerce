0ce28ddcc61052794078e1fe60a64d61d5adc662
LocCuTo <phananhloc03102001@gmail.com>
2022-11-21 16:10:18 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Header, Footer } from './components';
-import { Home, Contact, Login, Register, Reset, Admin, Cart } from './pages';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+// Pages
+import { Home, Contact, Login, Register, Reset, Admin } from './pages';
+// Components
+import { Header, Footer } from './components';
 import AdminOnlyRoute from './components/adminOnlyRoute/AdminOnlyRoute';
 import ProductDetails from './components/product/productDetails/ProductDetails';
+import Cart from './pages/cart/Cart';
 import CheckoutDetails from './pages/checkout/CheckoutDetails';
 import Checkout from './pages/checkout/Checkout';
 import CheckoutSuccess from './pages/checkout/CheckoutSuccess';
 import OrderHistory from './pages/orderHistory/OrderHistory';
 import OrderDetails from './pages/orderDetails/OrderDetails';
-import NotFound from './pages/notFound/NotFound';
 import ReviewProducts from './components/reviewProducts/ReviewProducts';
+import NotFound from './pages/notFound/NotFound';
 
 function App() {
     return (
@@ -26,15 +28,6 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/reset" element={<Reset />} />
-                    <Route path="/product-details/:id" element={<ProductDetails />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                    <Route path="/checkout-details" element={<CheckoutDetails />} />
-                    <Route path="/checkout-success" element={<CheckoutSuccess />} />
-                    <Route path="/order-history" element={<OrderHistory />} />
-                    <Route path="/order-details/:id" element={<OrderDetails />} />
-                    <Route path="/review-product/:id" element={<ReviewProducts />} />
-                    <Route path="*" element={<NotFound />} />
 
                     <Route
                         path="/admin/*"
@@ -44,6 +37,16 @@ function App() {
                             </AdminOnlyRoute>
                         }
                     />
+
+                    <Route path="/product-details/:id" element={<ProductDetails />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout-details" element={<CheckoutDetails />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/checkout-success" element={<CheckoutSuccess />} />
+                    <Route path="/order-history" element={<OrderHistory />} />
+                    <Route path="/order-details/:id" element={<OrderDetails />} />
+                    <Route path="/review-product/:id" element={<ReviewProducts />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
